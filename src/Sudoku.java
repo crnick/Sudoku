4c109febd805d0900f1b04c097e0be1df6cdbb51
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-05-08 06:37:15 -0400

    Created window, panel and text labels

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -6,7 +6,35 @@ import javax.swing.*;
 public class Sudoku {
     int boardWidth = 600;
     int boardHeight = 500;
-    JFrame frame = new JFrame("Sudoku");
+    JFrame frame = new JFrame("Sudoku"); //creates a window
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
+    JPanel
+
+    String[] puzzle = {
+            "--74916-5",
+            "2---6-3-9",
+            "-----7-1-",
+            "-586----4",
+            "--3----9-",
+            "--62--187",
+            "9-4-7---2",
+            "67-83----",
+            "81--45---"
+    };
+
+    String[] solution = {
+            "387491625",
+            "241568379",
+            "569327418",
+            "758619234",
+            "123784596",
+            "496253187",
+            "934176852",
+            "675832941",
+            "812945763"
+    };
 
     Sudoku(){
         frame.setVisible(true); //making window visible.
@@ -14,5 +42,16 @@ public class Sudoku {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null); // center the window.
+        frame.setLayout(new BorderLayout());//adding a layout where components can be placed NSEW
+
+        textLabel.setFont(new Font("Arial",Font.BOLD,30));
+        textLabel.setHorizontalAlignment(JLabel.CENTER); // set the alignment of the labels content.
+        textLabel.setText("Sudoku:0");
+
+        //attach label to panel
+        textPanel.add(textLabel);
+
+        //attach panel to the frame
+        frame.add(textPanel,BorderLayout.NORTH);
     }
 }
